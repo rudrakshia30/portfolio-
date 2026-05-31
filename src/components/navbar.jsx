39d85f778e5970163aa59abf98f063107cd1ee9c
rudrakshia30 <rudrakshia30@gmail.com>
2026-06-01 04:45:36 +0530

    navlink

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './navbar.css'
+import { NavLink } from "react-router-dom";
 const navbar = () => {
   return (
     <div className="navbar">
         <div className="logo">RA</div>
-    <ul class="navlinks">
-        <li><a href="#about">ABOUT</a></li>
-        <li><a href="#skills">SKILLS</a></li>
-        <li><a href="#github">GITHUB</a></li>
-        <li><a href="#projects">PROJECTS</a></li>
-        <li><a href="#contact">CONTACT</a></li>
+    <ul className="navlinks">
+        <li><NavLink to="/">ABOUT</NavLink></li>
+        <li><NavLink to="/skills">SKILLS</NavLink></li>
+        <li><NavLink to="/github">GITHUB</NavLink></li>
+        <li><NavLink to="/projects">PROJECTS</NavLink></li>
+        <li><NavLink to="/contact">CONTACT</NavLink></li>
     </ul>
 
     </div>

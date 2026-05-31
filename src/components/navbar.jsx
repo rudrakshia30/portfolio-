90099ae487a94c693921f4a337634fa3daf8fefa
rudrakshia30 <rudrakshia30@gmail.com>
2026-06-01 02:31:15 +0530

    navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div className="navbar">
+    <ul class="nav-links">
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#skills">SKILLS</a></li>
+        <li><a href="#github">GITHUB</a></li>
+        <li><a href="#projects">PROJECTS</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+    </ul>
+
+    </div>
+  )
+}
+
+export default navbar

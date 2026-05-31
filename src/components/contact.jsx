c040faba81f2be5bbce34c56353276e95b10f817
rudrakshia30 <rudrakshia30@gmail.com>
2026-06-01 02:21:51 +0530

    contact page

---
+++ src/components/contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>
+        <h1>
+        Let's build <br/>
+        something
+        </h1>
+        <p>
+        open to internship, collaborations, and intersting
+        problems. Drop a message- I usually respond fast.
+        </p>
+        <div className="butt">
+            <a href="#">EMAIL</a>
+            <a href="#">GITHUB</a>
+            <a href="#">LINKEDIN</a>
+        </div>
+        <footer>
+        <p>© 2025 — Designed & Developed by me</p>
+        <p>Aspiring Software & AI Engineer</p>
+        </footer>
+    </div>
+  )
+}
+
+export default contact

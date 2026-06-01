dc853b159f9fbf1220e7078dd04956102fe93bac
rudrakshia30 <rudrakshia30@gmail.com>
2026-06-01 14:31:22 +0530

    git style

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Contact from './components/contact'
 import About from './components/about'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Skill from './components/skills'
+import Git from "./components/git";
 function App() {
   return (
     <>
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/skills" element={<Skill />} />
+          <Route path="/github" element={<Git />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/git.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import "./git.css";
+function GithubStats() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("https://api.github.com/users/rudrakshia30")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  if (!data) return <h3>Loading...</h3>;
+  return (
+    <div className="stats">
+      <h2>GitHub Stats</h2>
+      <p><b>Username:</b> {data.login}</p>
+      <p><b>Public Repos:</b> {data.public_repos}</p>
+      <p><b>Followers:</b> {data.followers}</p>
+      <p><b>Following:</b> {data.following}</p>
+    </div>
+  );
+}
+export default GithubStats;

bff3b31f924f3fb106d8ed9af433a7dafd969afa
rudrakshia30 <rudrakshia30@gmail.com>
2026-06-06 03:38:12 +0530

    react icons import

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,55 +1,63 @@
 import React from 'react'
 import './skills.css'
-const skills = () => {
+
+import { FaPython, FaJs, FaReact, FaGitAlt, FaGithub, FaHtml5, FaCss3Alt } from "react-icons/fa";
+import { SiCplusplus, SiFigma, SiCanva } from "react-icons/si";
+
+const Skills = () => {
   return (
     <div>
-        <span className='sec'>SKILLS</span>
-        <h1>What I work with</h1>
-        <div className='grid'>
-            <div className='category'>
-                <h3>PROGRAMMING</h3>
-                <ul>
-                    <li>Python</li>
-                    <li>C/C++</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                </ul>
-            </div>
-            <div className='category'>
-                <h3>TOOLS</h3>
-                <ul>
-                    <li>Git & GitHub</li>
-                    <li>VS Code</li>
-                    <li>Figma</li>
-                    <li>Canva</li>
-                </ul>
-            </div>
-            <div className='category'>
-                <h3>CONCEPTS</h3>
-                <ul>
-                    <li>Data Structures</li>
-                    <li>Problem Solving</li>
-                    <li>OOPS (basic)</li>
-                </ul>
-            </div>
-            <div className='category'>
-                <h3>SOFT SKILLS</h3>
-                <ul>
-                    <li>Leadership</li>
-                    <li>Teamwork</li>
-                    <li>Critical Thinking</li>
-                    <li>Communication</li>
-                    <li>Time Management</li>
-                    <li>Multi-tasking</li>
-                    <li>Cooperation</li>
-                </ul>
-            </div>
+      <span className='sec'>SKILLS</span>
+      <h1>Technologies & Skills</h1>
+
+      <div className='grid'>
+        <div className='category'>
+          <h3>PROGRAMMING</h3>
+          <ul>
+            <li><FaPython /> Python</li>
+            <li><SiCplusplus /> C/C++</li>
+            <li><FaJs /> JavaScript</li>
+            <li><FaReact /> React</li>
+            <li><FaHtml5 /> HTML</li>
+            <li><FaCss3Alt /> CSS</li>
+          </ul>
         </div>
 
+        <div className='category'>
+          <h3>TOOLS</h3>
+          <ul>
+            <li><FaGitAlt /> Git</li>
+            <li><FaGithub /> GitHub</li>
+            <li>💻 VS Code</li>
+            <li><SiFigma /> Figma</li>
+            <li><SiCanva /> Canva</li>
+          </ul>
+        </div>
+
+        <div className='category'>
+          <h3>CONCEPTS</h3>
+          <ul>
+            <li>Data Structures</li>
+            <li>Problem Solving</li>
+            <li>OOPS basic</li>
+          </ul>
+        </div>
+
+        <div className='category'>
+          <h3>SOFT SKILLS</h3>
+          <ul>
+            <li>Leadership</li>
+            <li>Teamwork</li>
+            <li>Critical Thinking</li>
+            <li>Communication</li>
+            <li>Time Management</li>
+            <li>Multi-tasking</li>
+            <li>Cooperation</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default skills
+export default Skills

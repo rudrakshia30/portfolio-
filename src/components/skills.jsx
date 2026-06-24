546418f10c1d230179deab4e3983767ded7f3b5c
rudrakshia30 <rudrakshia30@gmail.com>
2026-06-24 20:22:39 +0530

    logosvskills

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,84 +1,237 @@
-import React from 'react'
-import './skills.css'
+import React from "react";
+import "./skills.css";
 
-import { FaPython, FaJs, FaReact, FaGitAlt, FaGithub, FaHtml5, FaCss3Alt } from "react-icons/fa";
-import { SiCplusplus, SiFigma, SiCanva } from "react-icons/si";
+import {
+  FaPython,
+  FaJs,
+  FaReact,
+  FaGitAlt,
+  FaGithub,
+  FaHtml5,
+  FaCss3Alt,
+  FaChartLine,
+  FaChartArea,
+  FaFileExcel,
+  FaBrain,
+  FaProjectDiagram,
+  FaLightbulb,
+  FaCubes,
+  FaUserTie,
+  FaUsers,
+  FaComments,
+  FaClock,
+  FaTasks,
+  FaHandshake,
+} from "react-icons/fa";
+
+import {
+  SiCplusplus,
+  SiFigma,
+  SiCanva,
+  SiFastapi,
+  SiPandas,
+  SiNumpy,
+  SiOpencv,
+} from "react-icons/si";
+
+import { VscCode } from "react-icons/vsc";
 
 const Skills = () => {
   return (
     <div>
-      <span className='sec'>SKILLS</span>
+      <span className="sec">SKILLS</span>
       <h1>Technologies & Skills</h1>
 
-      <div className='grid'>
-        <div className='category' data-aos="fade-up">
+      <div className="grid">
+        {/* Programming */}
+        <div className="category" data-aos="fade-up">
           <h3>PROGRAMMING</h3>
+
           <ul>
-            <li><FaPython /> Python</li>
-            <li><SiCplusplus /> C/C++</li>
-            <li><FaJs /> JavaScript</li>
-            <li><FaReact /> React</li>
-            <li><FaHtml5 /> HTML</li>
-            <li><FaCss3Alt /> CSS</li>
+            <li>
+              <FaPython />
+              Python
+            </li>
+
+            <li>
+              <SiCplusplus />
+              C/C++
+            </li>
+
+            <li>
+              <FaJs />
+              JavaScript
+            </li>
           </ul>
         </div>
-        <div className='category' data-aos="fade-up">
-          <h3>FRAMEWORK & TECHNOLOGIES</h3>
+
+        {/* Frameworks */}
+        <div className="category" data-aos="fade-up">
+          <h3>FRAMEWORKS & TECHNOLOGIES</h3>
+
           <ul>
-            <li>FastAPI</li>
-            <li><FaReact /> React</li>
-            <li><FaHtml5 /> HTML</li>
-            <li><FaCss3Alt /> CSS</li>
+            <li>
+              <SiFastapi />
+              FastAPI
+            </li>
+
+            <li>
+              <FaReact />
+              React
+            </li>
+
+            <li>
+              <FaHtml5 />
+              HTML
+            </li>
+
+            <li>
+              <FaCss3Alt />
+              CSS
+            </li>
           </ul>
         </div>
-        <div className='category' data-aos="fade-up">
+
+        {/* AI/ML */}
+        <div className="category" data-aos="fade-up">
           <h3>AI/ML & COMPUTER VISION</h3>
+
           <ul>
-            <li>Pandas</li>
-            <li>NumPY</li>
-            <li>MatPloLib</li>
-            <li>SeaBorn</li>
-            <li>OpenCV</li>
-            <li>LLM Intergeration</li>
+            <li>
+              <SiPandas />
+              Pandas
+            </li>
+
+            <li>
+              <SiNumpy />
+              NumPy
+            </li>
+
+            <li>
+              <FaChartLine />
+              Matplotlib
+            </li>
+
+            <li>
+              <FaChartArea />
+              Seaborn
+            </li>
+
+            <li>
+              <SiOpencv />
+              OpenCV
+            </li>
+
+            <li>
+              <FaBrain />
+              LLM Integration
+            </li>
           </ul>
         </div>
 
-        <div className='category' data-aos="fade-up">
+        {/* Developer Tools */}
+        <div className="category" data-aos="fade-up">
           <h3>DEVELOPER TOOLS</h3>
+
           <ul>
-            <li>Excel</li>
-            <li><FaGithub /> GitHub</li>
-            <li><FaGitAlt /> Git</li>
-            <li>💻 VS Code</li>
-            <li><SiFigma /> Figma</li>
-            <li><SiCanva /> Canva</li>
+            <li>
+              <FaFileExcel />
+              Excel
+            </li>
+
+            <li>
+              <FaGithub />
+              GitHub
+            </li>
+
+            <li>
+              <FaGitAlt />
+              Git
+            </li>
+
+            <li>
+              <VscCode />
+              VS Code
+            </li>
+
+            <li>
+              <SiFigma />
+              Figma
+            </li>
+
+            <li>
+              <SiCanva />
+              Canva
+            </li>
           </ul>
         </div>
 
-        <div className='category' data-aos="fade-up">
+        {/* Core Concepts */}
+        <div className="category" data-aos="fade-up">
           <h3>CORE CONCEPTS</h3>
+
           <ul>
-            <li>Data Structures and Algorithms</li>
-            <li>Problem Solving</li>
-            <li>OOPS basic</li>
+            <li>
+              <FaProjectDiagram />
+              Data Structures and Algorithms
+            </li>
+
+            <li>
+              <FaLightbulb />
+              Problem Solving
+            </li>
+
+            <li>
+              <FaCubes />
+              OOP Basics
+            </li>
           </ul>
         </div>
 
-        <div className='category'>
+        {/* Soft Skills */}
+        <div className="category" data-aos="fade-up">
           <h3>SOFT SKILLS</h3>
+
           <ul>
-            <li>Leadership</li>
-            <li>Teamwork</li>
-            <li>Critical Thinking</li>
-            <li>Communication</li>
-            <li>Time Management</li>
-            <li>Multi-tasking</li>
-            <li>Cooperation</li>
+            <li>
+              <FaUserTie />
+              Leadership
+            </li>
+
+            <li>
+              <FaUsers />
+              Teamwork
+            </li>
+
+            <li>
+              <FaBrain />
+              Critical Thinking
+            </li>
+
+            <li>
+              <FaComments />
+              Communication
+            </li>
+
+            <li>
+              <FaClock />
+              Time Management
+            </li>
+
+            <li>
+              <FaTasks />
+              Multitasking
+            </li>
+
+            <li>
+              <FaHandshake />
+              Cooperation
+            </li>
           </ul>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;

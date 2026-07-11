20c5946a89357be2368b3273c5dc076c04e7d1f1
rudrakshia30 <rudrakshia30@gmail.com>
2026-07-11 17:04:34 +0530

    navbar modification

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,14 +4,14 @@ import { NavLink } from "react-router-dom";
 const navbar = () => {
   return (
     <div className="navbar">
-        <div className="logo">RA</div>
-    <ul className="navlinks">
+      <div className="logo">RA</div>
+      <ul className="navlinks">
         <li><NavLink to="/">ABOUT</NavLink></li>
         <li><NavLink to="/skills">SKILLS</NavLink></li>
         <li><NavLink to="/github">GITHUB</NavLink></li>
         <li><NavLink to="/projects">PROJECTS</NavLink></li>
         <li><NavLink to="/contact">CONTACT</NavLink></li>
-    </ul>
+      </ul>
 
     </div>
   )

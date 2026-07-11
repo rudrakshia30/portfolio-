5d8d1dee21d063492598fd741ea30f77fd7d1332
rudrakshia30 <rudrakshia30@gmail.com>
2026-07-11 17:03:14 +0530

    contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -3,25 +3,25 @@ import './contact.css'
 const contact = () => {
   return (
     <div className='contact' data-aos="fade-up">
-        <h1>
-        Let's <span>build </span><br/>
+      <h1>
+        Let's <span>build </span><br />
         something.
-        </h1>
-        <p className='intro'>
+      </h1>
+      <p className='intro'>
         open to internship, collaborations, and intersting
-        problems.<br/> Drop a message- I usually respond fast.
-        </p>
-        <div className="butt">
-            <a href="https://mail.google.com/mail/?view=cm&fs=1&to=rudrakshia30@gmail.com"
-  target="_blank"
-  rel="noopener noreferrer">EMAIL</a>
-            <a href="https://github.com/rudrakshia30" target="_blank" rel="noopener noreferrer">GITHUB</a>
-            <a href="https://www.linkedin.com/in/rudrakshi-agarwal-264847379/" target="_blank" rel="noopener noreferrer">LINKEDIN</a>
-        </div>
-        <footer>
+        problems.<br /> Drop a message- I usually respond fast.
+      </p>
+      <div className="butt">
+        <a href="https://mail.google.com/mail/?view=cm&fs=1&to=rudrakshia30@gmail.com"
+          target="_blank"
+          rel="noopener noreferrer">EMAIL</a>
+        <a href="https://github.com/rudrakshia30" target="_blank" rel="noopener noreferrer">GITHUB</a>
+        <a href="https://www.linkedin.com/in/rudrakshi-agarwal-264847379/" target="_blank" rel="noopener noreferrer">LINKEDIN</a>
+      </div>
+      <footer>
         <p>© 2026 — Designed & Developed by me</p>
         <p>Aspiring Software & AI Engineer</p>
-        </footer>
+      </footer>
     </div>
   )
 }
